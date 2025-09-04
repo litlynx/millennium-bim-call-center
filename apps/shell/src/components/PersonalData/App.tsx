b78841127cb2d138c7c7d6405944e3e6681b070f
Thomas De Grava <thomas_alexandre96@hotmail.com>
2025-09-04 15:37:42 +0100

    feat: add file PersonalData/App.tsx

---
+++ apps/shell/src/components/PersonalData/App.tsx
@@ -0,0 +1,19 @@
+import type * as React from 'react';
+import { Card, CardItemLabel, Icon } from 'shared/components';
+import dataJson from './mock-data/mock-data.json';
+
+const PersonalData: React.FC = () => {
+  return (
+    <Card
+      icon={<Icon type="personal" className="bg-primary-500" />}
+      title="Dados Pessoais"
+      className="h-fit"
+    >
+      {Object.entries(dataJson as Record<string, string>).map(([label, value]) => (
+        <CardItemLabel key={label} title={label} text={value} />
+      ))}
+    </Card>
+  );
+};
+
+export default PersonalData;
