5fdc0faa13cbacc88f30ffcca63709011a85c4bb
Thomas De Grava <thomas_alexandre96@hotmail.com>
2025-09-05 14:53:48 +0100

    feat(PersonalData): add PersonalData component

--- apps/header-pages/src/Vision360/pages/Vision360Page.tsx
+++ apps/header-pages/src/Vision360/pages/Vision360Page.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router';
 import { Button } from 'shared/components';
+import PersonalData from 'src/components/PersonalData/PersonalData';
 
 export default function Vision360Page() {
   const navigate = useNavigate();
@@ -13,6 +14,7 @@ export default function Vision360Page() {
       <h1>Vision360 Page</h1>
       <Button onClick={() => navigate('/')}>Go back to dashboard root</Button>
       <Button onClick={() => navigate('another-level')}>Go to sub-level route</Button>
+      <PersonalData />
     </div>
   );
 }
