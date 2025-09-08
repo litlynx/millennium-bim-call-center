fa0a475922ee0e00593bc1a3b034b4dd5a914d07
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-09-08 18:11:51 +0100

    chore: change EstateAndProducts directory from components to Vision360

---
+++ apps/header-pages/src/Vision360/components/EstateAndProducts/types.ts
@@ -0,0 +1,20 @@
+export interface FinancialItemInterface {
+  name: string;
+  account?: string;
+  amount: string;
+  currency: string;
+}
+
+export interface FinancialSectionInterface {
+  title: string;
+  total: {
+    amount: string;
+    currency: string;
+  };
+  items: FinancialItemInterface[];
+}
+
+export interface EstateAndProductsData {
+  assets: FinancialSectionInterface;
+  liabilities: FinancialSectionInterface;
+}
