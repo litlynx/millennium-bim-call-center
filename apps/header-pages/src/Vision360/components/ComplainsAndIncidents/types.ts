9320ee011142c0a4938a0813bdc311ea7d4a7745
Thomas De Grava <thomas_alexandre96@hotmail.com>
2025-09-08 17:50:14 +0100

    feat(types): add types to components

---
+++ apps/header-pages/src/Vision360/components/ComplainsAndIncidents/types.ts
@@ -0,0 +1,14 @@
+export interface ClaimsProps {
+  number: string;
+  registerDate: string;
+  deadlineDate: string;
+  status: string;
+  amount: string;
+  type: string;
+}
+
+export interface IncidentsProps {
+  date: string;
+  type: string;
+  title: string;
+}
