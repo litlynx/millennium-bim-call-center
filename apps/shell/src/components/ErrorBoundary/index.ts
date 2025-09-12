b5e475ee441066274c777f738183d188d547ab93
Jorge Miranda <github@litlynx.com>
2025-09-12 16:19:18 +0100

    feat(error-boundary): added support for error boundary on failed lazy loading

---
+++ apps/shell/src/components/ErrorBoundary/index.ts
@@ -0,0 +1,9 @@
+export {
+  type CachedLazyComponent,
+  ComponentRegistry,
+  getComponentRegistry,
+  type LazyImportFunction,
+  registerComponent,
+  reloadComponent
+} from './ComponentRegistry';
+export { default, LazyErrorBoundary } from './ErrorBoundary';
