2a35d21c7c850d5441e0f89cfdbe4db298efcbcd
Jorge Miranda <github@litlynx.com>
2025-09-23 17:19:26 +0100

    feat(TextArea): implement useTextArea hook for form integration

--- packages/shared/src/components/TextArea/index.ts
+++ packages/shared/src/components/TextArea/index.ts
@@ -1,3 +1,3 @@
+export { useTextArea } from './hooks/useTextArea';
 export type { TextAreaProps } from './TextArea';
 export { default } from './TextArea';
-export { useTextArea } from './useTextArea';
