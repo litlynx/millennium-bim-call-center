2969b9e1384efe94eafb66b773cd3631bd4ecc28
Thomas De Grava <thomas_alexandre96@hotmail.com>
2025-09-23 09:56:09 +0100

    feat(component): create datepicker component

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -5,6 +5,7 @@ export { default as CardAccordion } from './Card/CardAccordion';
 export type { CardTabItem, CardTabsProps } from './Card/CardTabs';
 export { default as CardTabs } from './Card/CardTabs';
 export { default as CardItemLabel } from './CardItem/CardItemLabel';
+export { default as DatePicker } from './DatePicker/DatePicker';
 export type { IconProps } from './Icon/Icon';
 export { default as Icon } from './Icon/Icon';
 export { default as Input } from './Input/Input';
