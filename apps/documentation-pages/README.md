e8f5e35f408dc33a9cec9186747f8dc816667cb8
Jorge Miranda <github@litlynx.com>
2025-09-24 10:20:52 +0100

    [sc-132] create documentation-pages mfe (#46)

---
+++ apps/documentation-pages/README.md
@@ -0,0 +1,3 @@
+# Documentation Pages app
+
+Documentation Pages app serves the pages available in the documentation navigation from the sidebar-pages in the main application shell and consumes shared components from the `packages/shared/components` package.
---
+++ apps/documentation-pages/rspack/rspack.dev.ts
@@ -0,0 +1,25 @@
+import { Apps } from '@config/rspack-config/enums';
+import { getAppModuleFederationConfig } from '@config/rspack-config/module-federation';
+import getDevCommonConfig from '@config/rspack-config/rspack.dev';
+import * as rspack from '@rspack/core';
+import { merge } from 'webpack-merge';
+
+import getCommonConfig, { getCommonModuleFederationConfig } from './rspack.common';
+
+const getDevConfig = (): rspack.Configuration =>
+  merge(
+    getDevCommonConfig({
+      port: getAppModuleFederationConfig(Apps['documentation-pages']).devPort
+    }),
+    getCommonConfig(),
+    {
+      plugins: [
+        new rspack.container.ModuleFederationPlugin({
+          ...getCommonModuleFederationConfig(),
+          remotes: getAppModuleFederationConfig(Apps['documentation-pages']).remotes?.dev
+        })
+      ]
+    }
+  );
+
+export default getDevConfig;
