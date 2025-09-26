081e0cd5f57f59fe80bef5d829feaea756894a1a
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-09-26 11:01:11 +0100

    chore: refactor PrimaryTable and TransactionsTable components with mock data

---
+++ apps/records-pages/src/DigitalChannels/MobileBanking/mocks/mockPrimaryRows.ts
@@ -0,0 +1,18 @@
+export const mockPrimaryRows = [
+  {
+    id: 'row-1',
+    operatorName: 'TMcel',
+    phoneNumber: '825 816 811',
+    type: 'Principal',
+    stateSimSwap: 'Desbloqueado',
+    badgeText: 'Activo'
+  },
+  {
+    id: 'row-2',
+    operatorName: 'Vodacom',
+    phoneNumber: '845 816 811',
+    type: 'Secundário',
+    stateSimSwap: 'Desbloqueado',
+    badgeText: 'Activo'
+  }
+];
