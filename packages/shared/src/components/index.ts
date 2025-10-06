bf402b7f2418e43232035f8414c450ad1ac99513
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-10-06 16:52:11 +0100

    feat: add AuthenticationBadge component with authentication status display

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,5 +1,6 @@
 // Common UI components
 
+export { default as AuthenticationBadge } from './AuthenticationBadge/AuthenticationBadge';
 export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
 export { default as Button } from './Button/Button';
 export { default as ButtonDropdown } from './ButtonDropdown/ButtonDropdown';
